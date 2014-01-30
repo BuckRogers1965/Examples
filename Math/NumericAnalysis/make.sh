64fa749b4643832dbf2c8caf3be6fba53b635fbe
James M. Rogers <JamesMRogers@gmail.com>
2014-01-30 07:52:45 -0500

    Created a build script for homework

---
+++ Math/NumericAnalysis/make.sh
@@ -0,0 +1,10 @@
+ 
+
+gcc 02_homework02.c methods.c -o 02_homework02 -lm
+./02_homework02 > 02_homework02.results.txt
+
+gcc 02-6_hw.c                 -o 02-6_hw       -lm
+./02-6_hw > 02-6_hw.results.txt
+
+gcc 03_homework.c   methods.c -o 03_homework   -lm
+./03_homework > 03_homework.results.txt
